606be8f67586aec7c05cdc551c831f5d8199d636
Angel Medina <angel.medina95@hotmail.com>
2015-03-07 20:53:53 -0600

    Delete plot1.R

--- plot1.R
+++
@@ -1,5 +0,0 @@
-data <- read.csv("household_power_consumption.txt", header=TRUE, sep=";")[66638:69517,]
-data$Global_active_power<-as.numeric(as.character(data$Global_active_power))
-png("plot1.png")
-hist(data$Global_active_power, main="Global Active Power", col="red", xlab="Global Active Power (kilowatts)")
-dev.off()
